ae60b31a392132fee22bd1a345f6276fcce5eed3
Arthur Abogadil <arthur.abogadil@gmail.com>
2026-01-31 17:11:59 +0800

    Main function

---
+++ main_file.py
@@ -0,0 +1,9 @@
+def main(): 
+    print("Hello world from pytests!")
+    sec_function()
+
+def sec_function():
+    print("sec_function calleed after the main function")
+
+if __name__ == "__main__":
+    main()
