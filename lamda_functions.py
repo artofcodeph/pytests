3ca48961aa9726812d0b63ca6cb688cad3d7e7db
Arthur Abogadil <serdefa@gmail.com>
2026-02-07 19:39:40 +0800

    Added chained lambda functions

--- lamda_functions.py
+++ lamda_functions.py
@@ -8,3 +8,6 @@ sum_of_numbers_mult_by_two = reduce(lambda x, y: x+y, mult_by_two)
 print(f"List: {numbers}")
 print(f"mult_by_two: {mult_by_two}")
 print(f"sum_of_numbers_mult_by_two: {sum_of_numbers_mult_by_two}")
+
+sum_of_numbers_mult_by_two_shorthand = reduce(lambda x, y: x + y, map(lambda x: x * 2, numbers))
+print(f"The result of the chained function is: {sum_of_numbers_mult_by_two_shorthand}")
