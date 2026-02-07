be63571c69e3d917480b38cdbd05dd6375505503
Arthur Abogadil <serdefa@gmail.com>
2026-02-07 22:15:34 +0800

    more examples, sum of even and min-max

--- lamda_functions.py
+++ lamda_functions.py
@@ -11,3 +11,11 @@ print(f"sum_of_numbers_mult_by_two: {sum_of_numbers_mult_by_two}")
 
 sum_of_numbers_mult_by_two_shorthand = reduce(lambda x, y: x + y, map(lambda x: x * 2, numbers))
 print(f"The result of the chained function is: {sum_of_numbers_mult_by_two_shorthand}")
+
+# sum of all even numbers
+
+sum_of_ev = reduce(lambda x, y: x + y, filter(lambda x: x % 2 == 0, numbers))
+print(f"sum_of_ev: {sum_of_ev}")
+print(f"from {list(filter(lambda x: x % 2 == 0, numbers))}")
+
+print(f"max: {max(numbers)}, min: {min(numbers)}")
